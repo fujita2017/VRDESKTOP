bc2232487e15871a7d54ef0c32a19c0ddcd21ab3
fujita <fujita@smc-net.co.jp>
2018-11-27 17:29:53 +0900

    tesut

---
+++ Assets/Scripts/Delete.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delete : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(transform.position.z);
+        if (transform.position.z > 3 || transform.position.z < -3 || 
+            transform.position.y > 3 || transform.position.y < -3 || 
+            transform.position.z > 3 || transform.position.z < -3)
+        {
+            DestroyWin();
+        }
+	}
+    void DestroyWin()
+    {
+        //このScriptを削除する
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PinchCursor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinchCursor : MonoBehaviour
+{
+
+    public void pinchOn()
+    {
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            this.transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+
+    public void pinchOff()
+    {
+        for (int i = 0; i < transform.childCount; ++i)
+        {
+            this.transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+}
