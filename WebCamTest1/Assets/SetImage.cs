1d28aa9acedd2c70581aa95283bc3c391374226a
fujita <fujita@smc-net.co.jp>
2018-12-03 15:35:48 +0900

    カメラテスト

---
+++ WebCamTest1/Assets/SetImage.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class SetImage : MonoBehaviour
+{
+
+    public Image image;
+    private Sprite sprite;
+    private RectTransform hoge1;
+    private RectTransform hoge2;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        sprite = Resources.Load<Sprite>("1");
+        image = GameObject.Find("Image1").GetComponent<Image>();
+        image.sprite = sprite;
+        hoge1 = image.GetComponent<RectTransform>();
+
+
+        sprite = Resources.Load<Sprite>("2");
+        image = GameObject.Find("Image2").GetComponent<Image>();
+        image.sprite = sprite;
+        hoge2 = image.GetComponent<RectTransform>();
+
+//        hoge1.sizeDelta = new Vector2(2, 1);
+//        hoge1.localPosition = new Vector3(-5, 0, 0); //動いた！
+
+//        hoge2.sizeDelta = new Vector2(2, 1);
+//        hoge2.localPosition = new Vector3(-4, 0, 0);　//動いた！
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            hoge1.sizeDelta = new Vector2(4, 2);
+            hoge1.localPosition = new Vector3(-5, -3, 0);　//動いた！
+
+            hoge2.sizeDelta = new Vector2(4, 2);
+            hoge2.localPosition = new Vector3(-4, -3, 0);　//動いた！
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            hoge1.sizeDelta = new Vector2(20, 10);
+            hoge1.localPosition = new Vector3(0, 0, 0); //動いた！
+
+            hoge2.sizeDelta = new Vector2(4 ,2);
+            hoge2.localPosition = new Vector3(-4, 0, 0);　//動いた！
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            hoge1.sizeDelta = new Vector2(4, 2);
+            hoge1.localPosition = new Vector3(-5, 0, 0); //動いた！
+
+            hoge2.sizeDelta = new Vector2(4, 2);
+            hoge2.localPosition = new Vector3(0, 0, 0);　//動いた！
+        }
+
+    }
+}
---
+++ WebCamTest1/Assets/WebcamOverlay.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WebcamOverlay : MonoBehaviour
+{
+
+    public int Width = 1920;
+    public int Height = 1080;
+    public int FPS = 30;
+    public int CamDeviceNo = 1;
+    public float Alpha = 0.5f;
+
+    private RectTransform hoge;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        RawImage rawImage;
+        rawImage = this.GetComponents<RawImage>()[0];
+        hoge = rawImage.GetComponent<RectTransform>();
+
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        for (var i = 0; i < devices.Length; i++)
+        {
+            Debug.Log(i.ToString() + ":" + devices[i].name);
+        }
+
+        WebCamTexture webcamTexture = new WebCamTexture(devices[CamDeviceNo].name, Width, Height, FPS);
+        webcamTexture.Play();
+
+        rawImage.texture = webcamTexture;
+        rawImage.material.mainTexture = webcamTexture;
+        rawImage.color = new Color(rawImage.color.r, rawImage.color.g, rawImage.color.b, Alpha);
+
+//        hoge.sizeDelta = new Vector2(20, 10);
+//        hoge.localPosition = new Vector3(0, 0, 0);　//動いた！
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            hoge.sizeDelta = new Vector2(20, 10);
+            hoge.localPosition = new Vector3(0, 0, 0);　//動いた！
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            hoge.sizeDelta = new Vector2(4 ,2);
+            hoge.localPosition = new Vector3(-6, 0, 0);　//動いた！
+        }
+    }
+}
